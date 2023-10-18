a82b2ce0a3421848fab06233192fd9d6e3767de8
Kyriakos Kypraios <kyriakoskyp@gmail.com>
2023-10-18 08:18:35 +0300

    service using json-server

---
+++ src/app/app.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable, inject } from '@angular/core';
+import { Person } from './interfaces/person';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  constructor(private http: HttpClient = Inject(HttpClient)) { }
+
+  getAllUsers() {
+    return this.http.get<Person[]>('htttp://localhost:3000/users')
+  }
+
+  addUser(user: Person) {
+    return this.http.post<Person>('htttp://localhost:3000/users', user)
+  }
+}
