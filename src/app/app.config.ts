0fed122f61c6650ffc62a458b3fa0a4795bf262c
Kyriakos Kypraios <kkypraios@allweb.gr>
2023-11-01 06:58:57 +0200

    welcome component app.routes primer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,13 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(HttpClientModule),
-    provideAnimations()
-]
+    provideAnimations(),
+    provideRouter(routes),
+  ],
 };
