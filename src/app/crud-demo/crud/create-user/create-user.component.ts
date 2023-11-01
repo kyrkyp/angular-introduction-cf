1680b80a4eb8f4c08d4f794363e53fb644e29be4
Kyriakos Kypraios <kkypraios@allweb.gr>
2023-11-01 06:21:58 +0200

    Crud-user-form-component in create user

--- src/app/crud-demo/crud/create-user/create-user.component.ts
+++ src/app/crud-demo/crud/create-user/create-user.component.ts
@@ -3,19 +3,19 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormComponent } from 'src/app/reactive-form/reactive-form.component';
 import { Person } from 'src/app/interfaces/person';
 import { AppService } from 'src/app/app.service';
+import { CrudUserFormComponent } from '../../utils/crud-user-form/crud-user-form.component';
 
 @Component({
   selector: 'app-create-user',
   standalone: true,
-  imports: [CommonModule, ReactiveFormComponent],
+  imports: [CommonModule, ReactiveFormComponent, CrudUserFormComponent],
   templateUrl: './create-user.component.html',
-  styleUrls: ['./create-user.component.css']
+  styleUrls: ['./create-user.component.css'],
 })
 export class CreateUserComponent {
+  @Output() userCreated = new EventEmitter();
 
-  @Output() userCreated = new EventEmitter()
-
-  constructor(private appService: AppService = Inject(AppService)) { }
+  constructor(private appService: AppService = Inject(AppService)) {}
 
   onUser(user: Person) {
     this.appService.addUser(user).subscribe((user) => {
