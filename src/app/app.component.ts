18ad4029cbcfc0af58fcf4a4aff0ac55cfa66b59
Kyriakos Kypraios <kkypraios@allweb.gr>
2023-11-01 07:09:51 +0200

    Event Binding Porting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,71 +1,13 @@
-import { OutputDemoComponent } from './output-demo/output-demo.component';
-import { Component, Inject, OnInit, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { PersonAltComponent } from './person-alt/person-alt.component';
-import { PersonComponent } from './person/person.component';
-import { Person } from './interfaces/person';
-import { EventBindComponent } from './event-bind/event-bind.component';
-import { PersonCardComponent } from './person-card/person-card.component';
-import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
-import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
-import { AppService } from './app.service';
-import { CrudDemoComponent } from './crud-demo/crud-demo/crud-demo.component';
 import { ApplicationLayoutComponent } from './application-layout/application-layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    PersonComponent,
-    PersonAltComponent,
-    EventBindComponent,
-    OutputDemoComponent,
-    PersonCardComponent,
-    TemplateDrivenFormComponent,
-    ReactiveFormComponent,
-    CrudDemoComponent,
-    ApplicationLayoutComponent,
-  ],
+  imports: [CommonModule, ApplicationLayoutComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  name: string = 'Kyriakos';
-  lastname = 'Kypraios';
-
-  person: Person = {
-    givenName: 'Kyriakos',
-    surName: 'Kypraios',
-    age: 32,
-    email: 'test@email.com',
-    address: 'Athens, Greece',
-  };
-
-  users: Person[] = [];
-
-  sentUser: Person | undefined;
-
-  constructor(private appService: AppService = Inject(AppService)) {}
-
-  ngOnInit(): void {
-    this.appService.getAllUsers().subscribe((users) => {
-      this.users = users;
-      console.log(this.users);
-    });
-  }
-
-  onDeleteUser(index: number) {
-    this.users.splice(index, 1);
-  }
-
-  onSendUser(user: Person) {
-    console.log(user);
-    this.sentUser = user;
-  }
-
-  onNewPerson(person: Person) {
-    this.users.push(person);
-  }
-}
+export class AppComponent {}
--- src/app/event-bind/event-bind.component.ts
+++ src/app/event-bind/event-bind.component.ts
@@ -1,28 +1,37 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-event-bind',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './event-bind.component.html',
-  styleUrls: ['./event-bind.component.css']
+  styleUrls: ['./event-bind.component.css'],
 })
 export class EventBindComponent {
-  times = 0
-  userInput = 'Type something...'
+  times: number = 0;
+  userInput: string = 'Type Something';
 
   increaseCounter() {
-    this.times++
+    this.times += 1;
   }
 
   resetCounter() {
-    this.times = 0
+    this.times = 0;
   }
 
   onUserInput(event: Event) {
-    const target = event.target as HTMLInputElement
-
-    this.userInput = target.value
+    const target = event.target as HTMLInputElement;
+    this.userInput = target.value;
   }
 }
