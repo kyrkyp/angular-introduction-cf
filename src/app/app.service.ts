8799f6f4a3eaa3077747fc40a376550cd5eafa16
Kyriakos Kypraios <kkypraios@allweb.gr>
2023-11-01 06:14:52 +0200

    "Update user component"

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,25 +3,31 @@ import { Inject, Injectable, inject } from '@angular/core';
 import { Person } from './interfaces/person';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AppService {
-
-  constructor(private http: HttpClient = Inject(HttpClient)) { }
+  constructor(private http: HttpClient = Inject(HttpClient)) {}
 
   getAllUsers() {
-    return this.http.get<Person[]>('htttp://localhost:3000/users')
+    return this.http.get<Person[]>('htttp://localhost:3000/users');
   }
 
   addUser(user: Person) {
-    return this.http.post<Person>('htttp://localhost:3000/users', user)
+    return this.http.post<Person>('htttp://localhost:3000/users', user);
   }
 
   getUserById(id: number) {
-    return this.http.get<Person>('htttp://localhost:3000/users/' + id)
+    return this.http.get<Person>('htttp://localhost:3000/users/' + id);
   }
 
   deleteUserById(id: number) {
-    return this.http.delete<Person>('htttp://localhost:3000/users/' + id)
+    return this.http.delete<Person>('htttp://localhost:3000/users/' + id);
+  }
+
+  updateUser(user: Person) {
+    return this.http.put<Person>(
+      'htttp://localhost:3000/users/' + user.id,
+      user
+    );
   }
 }
